5fabc67506f7c283fd5bbb6d3229bae9ff24478a
eng19cs0336-tenzinngawang <69908667+eng19cs0336-tenzinngawang@users.noreply.github.com>
2021-02-15 15:55:38 +0530

    Update Activity5.c

--- Activity5.c
+++ Activity5.c
@@ -2,34 +2,34 @@
 
 int input()
 {
-  int a; 
-  printf("Enter a number\n");
-  scanf("%d",&a);
-  return a;
+	int a; 
+	printf("Enter a number\n");
+	scanf("%d",&a);
+	return a;
 }
 
 int find_vol(int h, int d, int b)
 {
-  int vol;
-  vol = ((h * d * b) + ( d / b))/3;
-  return vol;
+	int vol;
+	vol = ((h * d * b) + ( d / b))/3;
+	return vol;
 }
 
 void output(int h, int d, int b, int vol)
 {
-  printf("The volume of Tromboloid with Height: %d Depth: %d and Breadth: %d is %d\n", h, d,b, vol);
+	printf("The volume of Tromboloid with Height: %d Depth: %d and Breadth: %d is %d\n", h, d,b, vol);
 }
 
 int main()
 {
-  int h, d, b, vol;
-  printf("Input Height: ");
-  h = input();
+	int h, d, b, vol;
+	printf("Input Height: ");
+	h = input();
 	printf("Input Depth: ");
-  d = input();
-  printf("Input Breadth: ");
-  b = input();
-  vol = find_vol( h, d, b);
-  output( h, d, b, vol);
-  return 0;
+	d = input();
+	printf("Input Breadth: ");
+	b = input();
+	vol = find_vol( h, d, b);
+	output( h, d, b, vol);
+	return 0;
 }
